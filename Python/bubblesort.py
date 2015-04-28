9028623d270e1c848811de73850260f142153486
Felipe Morales <felipe.morales93@gmail.com>
2015-04-28 16:05:42 -0300

    Codigo python!

---
+++ Python/bubblesort.py
@@ -0,0 +1,2 @@
+def bubblesort(l):
+	
---
+++ Python/main.py
@@ -0,0 +1,8 @@
+from bubblesort import *
+from mergesort import *
+from quicksort import *
+from radixsort import *
+
+
+#Leer archivo y llevarlo todo a una lista
+#Hacer funcionar algoritmos y medir el tiempo
