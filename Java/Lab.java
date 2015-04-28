c587e6b36ce1c1242523089426a229a2be3ae383
Felipe Morales <felipe.morales93@gmail.com>
2015-04-28 17:21:26 -0300

    Funciona la hueá

--- Java/Lab.java
+++ Java/Lab.java
@@ -3,21 +3,55 @@ import java.util.*;
 
 public class Lab{
 	public static void main(String []args) {
-		List<Integer> numeros = new ArrayList<Integer>();
+		int[] numeros = new int[1000000];
 		try
 		{
-			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+			int i = 0;
+			BufferedReader reader = new BufferedReader(new FileReader("desordenado.in"));
 			String line;
 			while ((line = reader.readLine()) != null)
 			{
-				numeros.add(Integer.parseInt(line));
+				numeros[i] = Integer.parseInt(line);
+				//System.out.println(line);
+				i++;
 			}
 			reader.close();
+			System.out.println("Ordenar ahora...");
+			long startTime = System.currentTimeMillis();
+			Lab.bubbleSort(numeros);
+			long stopTime = System.currentTimeMillis();
+      		long elapsedTime = stopTime - startTime;
+      		
+			for(i = 0; i<1000000; i++){
+				System.out.println(line);
+			}
+			System.out.println("Elapsed time: "+Long.toString(elapsedTime));
 		}
 		catch(Exception e)
 		{
 			System.out.println("Error al abrir archivo");
 			e.printStackTrace();
 		}
+
 	}
+	public static void bubbleSort(int[] arr) {
+		boolean swapped = true;
+		int j = 0;
+		int tmp;
+		int it = 0;
+		while (swapped) {
+			it++;
+			System.out.println("Iteracion: "+Integer.toString(it));
+			swapped = false;
+			j++;
+			for (int i = 0; i < arr.length - j; i++) {
+				if (arr[i] > arr[i + 1]) {                          
+					tmp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = tmp;
+					swapped = true;
+				}
+			}                
+		}
+	}	
 }
